06f2c173674235af0100c03eeb8bcae9621311b6
Kate F <follett.katie@gmail.com>
2019-01-08 17:45:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,11 @@
   def Fizzbuzz(number)
     if (number % 3 == 0) && (number % 5 == 0)
       "Fizzbuzz"
+      elseif number % 5 == 0
+      "Buzz"
     elseif number % 3 == 0
       "Fizz"
-    elseif number % 5 == 0
-      "Buzz"
+    
     else
       number
     
