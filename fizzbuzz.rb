7930ac96456d1aa33078afeb5397a1050d5b85f2
Kate F <follett.katie@gmail.com>
2019-01-08 17:11:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
+class Fizzbuzz
+  def self.checker(number)
+    if number % 3 == 0 && number % 5 == 0
+      "Fizzbuzz"
+    elseif number % 3 == 0
+      "Fizz"
+    elseif number % 5 == 0
+      "Buzz"
+    else
+      number
+    end
+  end
+end
  
